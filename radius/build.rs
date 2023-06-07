b5357c96f7138dac563b61ab959ed9c38f99c9e4
aemmitt-ns <alkali+github@alkalinesecurity.com>
2023-06-07 00:10:06 -0400

    actually fix docs maybe?

--- radius/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    // if we are building docs, don't build boolector vendor
-    if std::env::var("DOCS_RS").is_ok() {
-        println!("cargo:rustc-env=BOOLECTOR_NO_VENDOR=1");
-    }
-}
